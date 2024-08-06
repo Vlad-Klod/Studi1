df9b8420ab5a6350c816ebc514f50e0ec80701d2
Владимир <vvklod1969@gmail.com>
2024-08-06 11:12:53 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -2,17 +2,20 @@ print(f'Задача - "Рассылка писем"')
 def send_email(message, recipient, sender="University.helo@gmail.com"):
 # Если строки recipient и sender не содержит "@" или не оканчивается на ".com"/".ru"/".net",
 # то вывести на экран(в консоль) строку: "Невозможно отправить письмо с адреса <sender> на адрес <recipient>".,
-    if ("@" or (".com" or ".ru" or ".net") not in  (recipient or sender)):
+    #if "@" not in  (recipient or sender) or (".com" or ".ru" or ".net") not in  (recipient or sender):
+    if ("@" not in recipient or not recipient.endswith((".com", ".ru", ".net"))
+            or "@" not in sender or not sender.endswith((".com", ".ru", ".net"))):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
+    elif sender == "University.helo@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    elif sender != "university.help@gmail.com":
+    else:
+        #elif sender != "university.help@gmail.com":
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
-#send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-#send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-#send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-#send_email("Поздравляю с началом учебного года!", "от Вована от души !")
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email("Поздравляю с началом учебного года!", "от Вована от души !")
