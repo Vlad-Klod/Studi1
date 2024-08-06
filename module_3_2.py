660fd2e2b7361ad0bc35f90b217e774b4cbdbca7
Владимир <vvklod1969@gmail.com>
2024-08-06 11:15:07 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -2,7 +2,6 @@ print(f'Задача - "Рассылка писем"')
 def send_email(message, recipient, sender="University.helo@gmail.com"):
 # Если строки recipient и sender не содержит "@" или не оканчивается на ".com"/".ru"/".net",
 # то вывести на экран(в консоль) строку: "Невозможно отправить письмо с адреса <sender> на адрес <recipient>".,
-    #if "@" not in  (recipient or sender) or (".com" or ".ru" or ".net") not in  (recipient or sender):
     if ("@" not in recipient or not recipient.endswith((".com", ".ru", ".net"))
             or "@" not in sender or not sender.endswith((".com", ".ru", ".net"))):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
@@ -11,7 +10,6 @@ def send_email(message, recipient, sender="University.helo@gmail.com"):
     elif sender == "University.helo@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
-        #elif sender != "university.help@gmail.com":
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
